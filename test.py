475978133b87c878a0c9718de954586bed704674
Parth Natekar <patnat26@gmail.com>
2022-12-01 18:39:28 -0800

    Added homodyne

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ import torch
 import sys
 sys.path.append('/home/parth/Projects/UCSD/Medical_Imaging/fastMRI')
 import fastmri
+from scipy.signal import hann
 # from fastmri.fftc import *
 # from fastmri.data import transforms as T
 
@@ -34,16 +35,28 @@ class PartialFourier:
 		else:
 			return ifftshift(ifft2(ifftshift(k_image)))
 
-	def maskFourier(self, k_image, mode='complex_conjugate'):
+	def maskFourier(self, k_image, p = 0.3, mode='complex_conjugate'):
 		masked_k_image = k_image.copy()
 
-		if mode == 'zero_fill':
-			masked_k_image[:, masked_k_image.shape[1]//2, :] = 0.
-		else:
-			for i in range(1, masked_k_image.shape[1]//2):
+		masked_k_image[:, (int((1-p)*masked_k_image.shape[1]) + 1):, :] = 0.
+
+		if mode == 'complex_conjugate':
+			for i in range(1, int(p*masked_k_image.shape[1])):
 				for j in range(-masked_k_image.shape[2]//2 + 1, masked_k_image.shape[2]//2):
-					masked_k_image[:, 320 + i, masked_k_image.shape[2]//2 + j] = np.conjugate(masked_k_image[:, 320 - i, masked_k_image.shape[2]//2 - j])
+					masked_k_image[:, int((1-p)*masked_k_image.shape[1]) + i, masked_k_image.shape[2]//2 + j] = np.conjugate(masked_k_image[:, int(p*masked_k_image.shape[1]) - i, masked_k_image.shape[2]//2 - j])
+		elif mode == 'homodyne':
+			masked_k_image = self.getHomodyne(masked_k_image, p)
 		return masked_k_image
+
+	def getHomodyne(self, k_image, p):
+		symmetric_window = int((0.5-p)*k_image.shape[1]) + 1
+
+		homodyne_filter = np.ones(k_image.shape)
+
+		homodyne_filter[:, :k_image.shape[1]//2-symmetric_window] = 2.
+		homodyne_filter[:, k_image.shape[1]//2+symmetric_window:] = 0.
+
+		return k_image*homodyne_filter
 
 	def plot(self, image):
 		if self.mode == 'fastmri':
@@ -53,14 +66,28 @@ class PartialFourier:
 		plt.imshow(slice_image_abs, cmap='Greys_r')
 		plt.show()
 
-	def getPartialFourier(self, image):
+	def getPartialFourier(self, image, p=0.4, mode='complex_conjugate'):
 		fourier = self.getFourier(image)
-		masked_fourier = self.maskFourier(fourier, mode='zero_fill')
+		masked_fourier = self.maskFourier(fourier, p=p, mode=mode)
 		reconstructed = self.getFourierInverse(masked_fourier)
+		if mode == 'homodyne':
+			phase = self.getPhase(masked_fourier, p)
+			reconstructed = np.real(np.conjugate(phase) * reconstructed)
 		return reconstructed, self.getDifference(image, reconstructed)
 
 	def getDifference(self, image1, image2):
 		return image1 - image2, np.linalg.norm(image1 - image2)
+
+	def getPhase(self, k_image, p):
+		symmetric_window = int((0.5-p)*k_image.shape[1]) + 1
+
+		phase = np.zeros(k_image.shape, dtype=np.complex_)
+		phase[:, k_image.shape[1]//2-symmetric_window:k_image.shape[1]//2+symmetric_window, :] = \
+		k_image[:, k_image.shape[1]//2-symmetric_window:k_image.shape[1]//2+symmetric_window, :] 
+		
+		phase = np.exp(1j * np.angle(self.getFourierInverse(phase)))
+
+		return phase
 
 if __name__ == '__main__':
 	P = PartialFourier('/media/parth/DATA/datasets/fastMRI/singlecoil_challenge/file1000054.h5', 
@@ -68,9 +95,11 @@ if __name__ == '__main__':
 	image = P.getFourierInverse(P.image_file)
 	real_image = np.real(image)
 
-	reconstructed_image, diff = P.getPartialFourier(image)
-	reconstructed_real_image, real_diff = P.getPartialFourier(real_image)
+	mode = 'homodyne'
+
+	reconstructed_image, diff = P.getPartialFourier(image, mode = mode)
+	reconstructed_real_image, real_diff = P.getPartialFourier(real_image, mode=mode)
 	
 	print(diff[1], real_diff[1])
 
-	P.plot(diff[0][15])
+	P.plot(real_diff[0][15])
